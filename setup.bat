684ea5a34473d9a9300014be5ec78d9b52bbd7b1
Toshiyuki Kurooka <kurooka@webtech.co.jp>
2016-02-16 19:30:16 +0900

    no message

--- setup.bat
+++ setup.bat
@@ -6,15 +6,19 @@ if exist "depot_tools\" (
 	call :gitclone
 )
 
-exit /b
+goto :END
 
 rem =================================================
 rem subrutine
 rem =================================================
+
 :gitclone
+
 echo "clone for google depot tools"
 git clone https://chromium.googlesource.com/chromium/tools/depot_tools.git
 set PATH=%PATH%;%CD%\depot_tools
 fetch v8
 exit /b
 
+:END
+pause
